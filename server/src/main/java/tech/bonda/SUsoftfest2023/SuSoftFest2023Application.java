4b8052314796044752f6f1c060d41a3886acc27b
bonda29 <74877107+bonda29@users.noreply.github.com>
2023-10-25 11:36:03 +0300

    Add files via upload

---
+++ server/src/main/java/tech/bonda/SUsoftfest2023/SuSoftFest2023Application.java
@@ -0,0 +1,13 @@
+package tech.bonda.SUsoftfest2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuSoftFest2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuSoftFest2023Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/tech/bonda/SUsoftfest2023/SuSoftFest2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bonda.SUsoftfest2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuSoftFest2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
