46132b604f547c9fa306808351781698997dd2bc
bonda29 <bondableglobe10@gmail.com>
2023-10-28 22:44:40 +0300

    cors2

--- server/src/main/java/tech/bonda/sufest2023/config/CORSConfig.java
+++ server/src/main/java/tech/bonda/sufest2023/config/CORSConfig.java
@@ -1,19 +1,26 @@
 package tech.bonda.sufest2023.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc // Enable CORS globally
-public class CORSConfig implements WebMvcConfigurer {
+public class CORSConfig { //implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*"); // Allow all headers
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
